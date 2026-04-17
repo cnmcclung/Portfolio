4019af0e98b59d6946867e0d8822a8055ff20802
cnmcclung <cnmcclung@ucsd.edu>
2026-04-17 15:13:01 -0700

    fix nav

--- global.js
+++ global.js
@@ -3,3 +3,14 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Get all nav links
+let navLinks = $$("nav a");
+
+// Find the current page link
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Add the class (safe version)
+currentLink?.classList.add("current");
