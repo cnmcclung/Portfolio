49bc4a953faacc0c6f96e1428b2c52dae288c266
cnmcclung <cnmcclung@ucsd.edu>
2026-04-17 19:23:36 -0700

    pls

--- global.js
+++ global.js
@@ -66,16 +66,16 @@ for (let p of pages) {
 
 // currentLink?.classList.add("current");
 
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  `
-  <label class="color-scheme">
-    Theme:
-    <select id="theme-select">
-      <option value="light dark">Automatic</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-    </select>
-  </label>
-  `
-);
+// document.body.insertAdjacentHTML(
+//   "afterbegin",
+//   `
+//   <label class="color-scheme">
+//     Theme:
+//     <select id="theme-select">
+//       <option value="light dark">Automatic</option>
+//       <option value="light">Light</option>
+//       <option value="dark">Dark</option>
+//     </select>
+//   </label>
+//   `
+// );
