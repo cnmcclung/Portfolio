5bac9ef5c1eef889efb74e1798a751e087f028f4
cnmcclung <cnmcclung@ucsd.edu>
2026-04-17 19:39:00 -0700

    should be all good now

--- global.js
+++ global.js
@@ -79,3 +79,17 @@ for (let p of pages) {
 //   </label>
 //   `
 // );
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  
+  <label class="color-scheme">
+    Theme:
+    <select id="theme-select">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  
+);
