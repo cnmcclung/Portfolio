eb2db8c5df57d5778b1e6f769e9fc6c15fb71383
cnmcclung <cnmcclung@ucsd.edu>
2026-04-22 17:13:55 -0700

    start of lab 4

--- global.js
+++ global.js
@@ -106,3 +106,33 @@ select.addEventListener("change", () => {
   document.documentElement.style.colorScheme = value;
   localStorage.setItem("theme", value);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
