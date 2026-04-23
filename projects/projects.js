fd56ec493d82475941d29dd99cc04fa3fa26bea0
cnmcclung <cnmcclung@ucsd.edu>
2026-04-22 19:44:36 -0700

    fix

--- projects/projects.js
+++ projects/projects.js
@@ -1,18 +1,16 @@
-
 import { fetchJSON, renderProjects } from '../global.js';
+
 const projects = await fetchJSON('../lib/projects.json');
+
+// select container
 const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
-
-// 🔢 count projects
-const count = projects.length;
-
-// 🎯 select the title element
-const title = document.querySelector('.projects-title');
-
-// 📝 update the text
-title.textContent = `Projects (${count})`;
 
 // render projects
-const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+// count projects
+const count = projects.length;
+
+// update title
+const title = document.querySelector('.projects-title');
+title.textContent = `Projects (${count})`;
