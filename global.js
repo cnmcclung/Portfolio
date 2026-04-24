96a48c743d090a1dc5639c1c73e9287ba56f6d08
cnmcclung <cnmcclung@ucsd.edu>
2026-04-24 13:49:40 -0700

    github

--- global.js
+++ global.js
@@ -166,3 +166,8 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.appendChild(article);
   });
 }
+
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/cnmcclung`);
+}
