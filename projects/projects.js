fabfac5ccfc57e33b5342e1e22e32fef52c18293
cnmcclung <cnmcclung@ucsd.edu>
2026-04-30 18:29:14 -0700

    using d3

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -14,3 +16,10 @@ const count = projects.length;
 // update title
 const title = document.querySelector('.projects-title');
 title.textContent = `Projects (${count})`;
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
