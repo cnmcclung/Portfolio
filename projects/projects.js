6d1e08038f4063f743d257ec4bab38906a7874bd
cnmcclung <cnmcclung@ucsd.edu>
2026-04-30 19:39:30 -0700

    locked

--- projects/projects.js
+++ projects/projects.js
@@ -53,14 +53,18 @@ title.textContent = `Projects (${count})`;
 //     .attr('fill', colors[idx]);
 // });
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
+
+
 
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
@@ -89,3 +93,5 @@ data.forEach((d, idx) => {
     .attr('class', 'legend-item')
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
+
+
